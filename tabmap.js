4ad1891c2297c6484a6124d6c8fa45b0ed1c54d5
Itamar Givon <itamar.givon@wikimedia.de>
2022-04-25 15:52:34 +0200

    Add popup

--- tabmap.js
+++ tabmap.js
@@ -1,1 +1,15 @@
-document.body.style.border = "5px solid red";
+(function() {
+    /**
+     * Check and set a global guard variable.
+     * If this content script is injected into the same page again,
+     * it will do nothing next time.
+     */
+    if (window.hasRun) {
+      return;
+    }
+    window.hasRun = true;
+
+    console.log('loaded tabmap.js');
+
+  })();
+  
